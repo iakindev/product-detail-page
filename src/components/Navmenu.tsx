02b41e4f0b97a220c1794b0c44c676546d96255e
İlyas <ashnwor@gmail.com>
2020-09-15 20:57:09 +0300

    misc: apply responsive css to li instad of button

--- src/components/Navmenu.tsx
+++ src/components/Navmenu.tsx
@@ -4,8 +4,8 @@ import { Menu } from './icons';
 const Navmenu: React.FC = ({ children }) => (
   <ul className="ml-auto list-none flex flex-wrap justify-between space-x-6 overflow-hidden">
     {children}
-    <li className="w-auto">
-      <button className="inline-block md:hidden text-2xl h-full text-nav-gray">
+    <li className="inline-block md:hidden w-auto">
+      <button className="text-2xl h-full text-nav-gray">
         <Menu />
       </button>
     </li>
