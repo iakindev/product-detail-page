6f0ba8dfaaba8652f2cc3bf0aa4a4d617d2aec90
İlyas <ashnwor@gmail.com>
2020-09-17 01:24:36 +0300

    general: add to cart button functionality

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -26,8 +26,15 @@ const Details: React.FC<Props> = ({ product }) => {
       <Price price={419.99} discount={{ rate: 33, price: 279.99 }} />
       <AvailableSizes sizes={product.availableSizes} state={{ size, setSize }} />
       <button
-        className="bg-purple-700 text-xl px-10 py-4 rounded-lg text-white font-semibold focus:outline-none hover:bg-purple-600 mt-5"
-        onClick={() => alert(JSON.stringify(product, null, 4))}
+        className={`bg-purple-700 text-xl px-10 py-4 rounded-lg text-white font-semibold focus:outline-none mt-5 
+        ${!size ? 'opacity-50 cursor-not-allowed' : 'hover:bg-purple-600'}`}
+        onClick={() => {
+          if (size) {
+            alert(JSON.stringify({ id: product.productId, seller: product.seller, size }, null, 4));
+          } else {
+            alert('Please select a size to continue');
+          }
+        }}
       >
         Add to Cart
       </button>
