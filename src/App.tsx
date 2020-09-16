956d49a76730414935d7f08f53f616f21a47384e
İlyas <ashnwor@gmail.com>
2020-09-16 14:31:59 +0300

    general: make images fade in

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import CategoryBar from './components/CategoryBar';
 import ProductPage from './components/ProductPage';
 import { CarouselProvider, Slide, Slider, Dot } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
+import ImageFadeIn from 'react-image-fade-in';
 
 function App() {
   return (
@@ -37,47 +38,41 @@ function App() {
         <CarouselProvider naturalSlideWidth={10} naturalSlideHeight={10} totalSlides={3}>
           <Slider className="rounded-xl sm:w-full md:w-7/12 shadow-2xl mt-4">
             <Slide index={0} className="flex items-center">
-              <img
+              <ImageFadeIn
                 src="https://www.dogostore.com/images/thumbs/0012583_lost-in-space.jpg"
-                alt=""
                 className="object-contain w-full h-full inline-block"
               />
             </Slide>
             <Slide index={1} className="flex items-center">
-              <img
+              <ImageFadeIn
                 src="https://www.dogostore.com/images/thumbs/0012582_lost-in-space.jpg"
-                alt=""
                 className="object-contain w-full h-full inline-block"
               />
             </Slide>
             <Slide index={2}>
-              <img
+              <ImageFadeIn
                 src="https://www.dogostore.com/images/thumbs/0012584_lost-in-space.jpg"
-                alt=""
                 className="object-contain w-full h-full inline-block"
               />
             </Slide>
           </Slider>
           <div className="flex justify-between sm:w-full md:w-7/12">
             <Dot slide={0} className="mt-8 w-40 h-40 rounded-lg overflow-hidden shadow-2xl">
-              <img
+              <ImageFadeIn
                 src="https://www.dogostore.com/images/thumbs/0012583_lost-in-space.jpg"
-                alt=""
-                className="w-full"
+                className="object-contain w-full h-full inline-block"
               />
             </Dot>
             <Dot slide={1} className="mt-8 w-40 h-40 rounded-lg overflow-hidden shadow-2xl">
-              <img
+              <ImageFadeIn
                 src="https://www.dogostore.com/images/thumbs/0012582_lost-in-space.jpg"
-                alt=""
-                className="w-full"
+                className="object-contain w-full h-full inline-block"
               />
             </Dot>
             <Dot slide={2} className="mt-8 w-40 h-40 rounded-lg overflow-hidden shadow-2xl">
-              <img
+              <ImageFadeIn
                 src="https://www.dogostore.com/images/thumbs/0012584_lost-in-space.jpg"
-                alt=""
-                className="w-full"
+                className="object-contain w-full h-full inline-block"
               />
             </Dot>
           </div>
