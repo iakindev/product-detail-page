790a21305e357b4a0071f83397652a3f17797fdd
İlyas <ashnwor@gmail.com>
2020-09-16 03:52:33 +0300

    general: ugly initial product page code

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import Cart from './components/Cart';
 import Profile from './components/Profile';
 import CategoryBar from './components/CategoryBar';
 import ProductPage from './components/ProductPage';
+import { CarouselProvider, Slide, Slider, Dot } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 function App() {
   return (
@@ -32,6 +34,56 @@ function App() {
       <ProductPage>
         {/* In production, I would get trail property from history from React Router */}
         <CategoryBar trail={['home', 'men', 'sneaker']} />
+        <CarouselProvider naturalSlideWidth={10} naturalSlideHeight={10} totalSlides={3}>
+          <Slider className="rounded-xl sm:w-full md:w-7/12 shadow-2xl mt-4">
+            <Slide index={0} className="flex items-center">
+              <img
+                src="https://www.dogostore.com/images/thumbs/0012583_lost-in-space.jpg"
+                alt=""
+                className="object-contain w-full h-full inline-block"
+              />
+            </Slide>
+            <Slide index={1} className="flex items-center">
+              <img
+                src="https://www.dogostore.com/images/thumbs/0012582_lost-in-space.jpg"
+                alt=""
+                className="object-contain w-full h-full inline-block"
+              />
+            </Slide>
+            <Slide index={2}>
+              <img
+                src="https://www.dogostore.com/images/thumbs/0012584_lost-in-space.jpg"
+                alt=""
+                className="object-contain w-full h-full inline-block"
+              />
+            </Slide>
+          </Slider>
+          <div className="flex justify-between sm:w-full md:w-7/12">
+            <Dot slide={0} className="mt-8 w-40 h-40 rounded-lg overflow-hidden shadow-2xl">
+              <img
+                src="https://www.dogostore.com/images/thumbs/0012583_lost-in-space.jpg"
+                alt=""
+                className="w-full"
+              />
+            </Dot>
+            <Dot slide={1} className="mt-8 w-40 h-40 rounded-lg overflow-hidden shadow-2xl">
+              <img
+                src="https://www.dogostore.com/images/thumbs/0012582_lost-in-space.jpg"
+                alt=""
+                className="w-full"
+              />
+            </Dot>
+            <Dot slide={2} className="mt-8 w-40 h-40 rounded-lg overflow-hidden shadow-2xl">
+              <img
+                src="https://www.dogostore.com/images/thumbs/0012584_lost-in-space.jpg"
+                alt=""
+                className="w-full"
+              />
+            </Dot>
+          </div>
+          {/* <ButtonBack>Back</ButtonBack>
+          <ButtonNext>Next</ButtonNext> */}
+        </CarouselProvider>
       </ProductPage>
     </Container>
   );
